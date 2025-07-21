9a23c35c3a301b3a0128f6f4972f3c213ba51493
Erenay Karacan <erenaykaracan@student-net-vpn-0013.intern.ethz.ch>
2025-07-21 23:11:35 +0200

    add MPS cluster scipts

--- examples/TFIM2d/cluster_script_MPS_1.py
+++ examples/TFIM2d/cluster_script_MPS_1.py
@@ -27,16 +27,6 @@ initial_state_BD, exact_state_BD, ccU_BD = (2**2, 2**11, 2**10) # Bond dimension
 Lx, Ly = (6, 6)
 L= Lx*Ly
 
-# construct Hamiltonian
-latt = qib.lattice.IntegerLattice((Lx, Ly), pbc=True)
-field = qib.field.Field(qib.field.ParticleType.QUBIT, latt)
-J, h, g = (1, 0, 3)
-hamil = qib.IsingHamiltonian(field, J, h, g).as_matrix()
-eigenvalues, eigenvectors = scipy.sparse.linalg.eigsh(hamil, k=10)
-idx = eigenvalues.argsort()
-eigenvalues_sort = eigenvalues[idx]
-eigenvectors_sort = eigenvectors[:,idx]
-ground_state = eigenvectors_sort[:, 0]
 
 perms_v, perms_h = get_perms(Lx, Ly)
 
