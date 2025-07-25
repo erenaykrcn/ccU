bcae11d37133cf2e921812d9138a10834c91f293
Erenay Karacan <erenaykaracan@student-net-vpn-0117.intern.ethz.ch>
2025-07-25 22:40:09 +0200

    updated XY 27 layers script

--- examples/triangularXY/cluster_script_XY_27.py
+++ examples/triangularXY/cluster_script_XY_27.py
@@ -18,43 +18,27 @@ from scipy.linalg import expm
 
 Lx, Ly = (4, 4)
 L = Lx*Ly
-t = 0.125
+t = 0.25
 # construct Hamiltonian
 latt = qib.lattice.TriangularLattice((Lx, Ly), pbc=True)
 field = qib.field.Field(qib.field.ParticleType.QUBIT, latt)
-J = (1, 1, 1)
-h = (3, -1, 1)
+J = (0, 1, 1)
+h = (3, 0, 1)
 hamil = qib.HeisenbergHamiltonian(field, J, h).as_matrix()
-#eigenvalues, eigenvectors = scipy.sparse.linalg.eigsh(hamil, k=10)
-#idx = eigenvalues.argsort()
-#eigenvalues_sort = eigenvalues[idx]
-#eigenvectors_sort = eigenvectors[:,idx]
-#ground_state = eigenvectors_sort[:, 0]
+eigenvalues, eigenvectors = scipy.sparse.linalg.eigsh(hamil, k=10)
+idx = eigenvalues.argsort()
+eigenvalues_sort = eigenvalues[idx]
+eigenvectors_sort = eigenvectors[:,idx]
+ground_state = eigenvectors_sort[:, 0]
+
 perms_1 = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], [1, 2, 3, 0, 5, 6, 7, 4, 9, 10, 11, 8, 13, 14, 15, 12]]
 perms_2 = [[0, 5, 10, 15, 3, 4, 9, 14, 2, 7, 8, 13, 1, 6, 11, 12], [5, 10, 15, 0, 4, 9, 14, 3, 7, 8, 13, 2, 6, 11, 12, 1]]
 perms_3 = [[0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15], [4, 8, 12, 0, 5, 9, 13, 1, 6, 10, 14, 2, 7, 11, 15, 3]]
 
+with h5py.File(f"./results/triangularXY_ccU_SPARSE_011_h301_Lx4Ly4_t0.25_layers27_niter5_rS1_2hloc.hdf5", "r") as f:
+    Vlist =  f["Vlist"][:]
 
-hloc1 = construct_heisenberg_local_term((J[0], 0   ,    0), (0, h[1],       0), ndim=3)
-hloc2 = construct_heisenberg_local_term((0   ,    J[1], 0), (0, 0, h[2]      ), ndim=3)
-hloc3 = construct_heisenberg_local_term((0   , 0   , J[2]), (h[0], 0,       0), ndim=3)
-
-V1 = scipy.linalg.expm(-1j*t*hloc1)
-V2 = scipy.linalg.expm(-1j*t*hloc2)
-V3 = scipy.linalg.expm(-1j*t*hloc3)
-
-X = np.array([[0, 1], [1, 0]])
-Z = np.array([[1, 0], [0, -1]])
-Y = np.array([[0, -1j], [1j, 0]])
-I2 = np.array([[1, 0], [0, 1]])
-XZ = np.kron(X, Z)
-XY = np.kron(X, Y)
-ZY = np.kron(Z, Y)
-Vlist_start = [XZ, V1, XZ]*3 + [XY, V2, XY]*3 + [ZY, V3, ZY]*3
-Vlist_reduced = [V1]*3 + [V2]*3  + [V3]*3
-
-perms_extended = [[perms_1[0]]] + [perms_1] + [[perms_1[0]], [perms_2[0]]] +\
-      [perms_2] + [[perms_2[0]], [perms_3[0]]] +  [perms_3] + [[perms_3[0]]]
+perms_extended = [[perms_1[0]]] + [perms_1] + [[perms_1[0]], [perms_2[0]]] +  [perms_2] + [[perms_2[0]], [perms_3[0]]] +  [perms_3] + [[perms_3[0]]]
 perms_extended = perms_extended*3
 perms_ext_reduced = [perms_1] + [perms_2] + [perms_3]
 perms_ext_reduced = perms_ext_reduced*3
@@ -63,6 +47,12 @@ control_layers = []
 for i in range(27):
     if i not in non_control_layers:
         control_layers.append(i)
+
+Vlist_reduced = []
+for i, V in enumerate(Vlist):
+    if i not in control_layers:
+        Vlist_reduced.append(V)
+
 
 state = random_statevector(2**L).data
 print("Trotter error of the starting point: ", (np.linalg.norm(ansatz_sparse(Vlist_start, L, perms_extended, state) - expm_multiply(
@@ -77,8 +67,9 @@ print('fidelity: ', (state_fidelity(ansatz_sparse(Vlist_start, L, perms_extended
 
 from optimize_sparse import optimize, err
 
-Vlist, f_iter, err_iter = optimize(L, hamil, t, Vlist_start, perms_extended, perms_reduced=perms_ext_reduced,
-                                   control_layers=control_layers, rS=1, niter=5)
+Vlist_start = Vlist
+Vlist, f_iter, err_iter = optimize(L, hamil, t, Vlist_start, perms_extended, perms_reduced=perms_ext_reduced, 
+                                   control_layers=control_layers, rS=1, niter=10)
 plt.plot(err_iter)
 plt.yscale('log')
 print(err_iter[-1])
@@ -90,3 +81,4 @@ with h5py.File(f"./triangularXY_ccU_SPARSE_{J}{h}{g}_Lx{Lx}Ly{Ly}_t{t}_layers{le
     f.create_dataset("err_iter", data=err_iter)
     f.attrs["L"] = L
     f.attrs["t"] = float(t)
+
