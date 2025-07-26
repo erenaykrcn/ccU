b803e10058eb21cd48b44507602dcaceee5b5007
Erenay Karacan <erenaykaracan@student-net-vpn-0312.intern.ethz.ch>
2025-07-26 22:25:20 +0200

    add the 1/3 compression plot

---
+++ examples/TFIM2d/cluster_10layers_023.py
@@ -0,0 +1,89 @@
+"""
+    (Successful) Attempt for the controlled version:
+"""
+import numpy as np
+import qiskit
+from qiskit.quantum_info import state_fidelity
+from numpy import linalg as LA
+import qib
+import matplotlib.pyplot as plt
+import scipy
+import h5py
+
+import sys
+sys.path.append("../../src/brickwall_sparse")
+from utils_sparse import construct_ising_local_term, reduce_list, X, I2, get_perms
+from ansatz_sparse import ansatz_sparse
+import rqcopt as oc
+from scipy.sparse.linalg import expm_multiply
+from qiskit.quantum_info import random_statevector
+
+Lx, Ly = (4, 4)
+L = Lx*Ly
+t = .23
+latt = qib.lattice.IntegerLattice((Lx, Ly), pbc=True)
+field = qib.field.Field(qib.field.ParticleType.QUBIT, latt)
+J, h, g = (1, 0, 3)
+hamil = qib.IsingHamiltonian(field, J, h, g).as_matrix()
+eigenvalues, eigenvectors = scipy.sparse.linalg.eigsh(hamil, k=10)
+idx = eigenvalues.argsort()
+eigenvalues_sort = eigenvalues[idx]
+eigenvectors_sort = eigenvectors[:,idx]
+ground_state = eigenvectors_sort[:, 0]
+
+X = np.array([[0, 1], [1, 0]])
+Z = np.array([[1, 0], [0, -1]])
+Y = np.array([[0, -1j], [1j, 0]])
+I2 = np.array([[1, 0], [0, 1]])
+
+hloc1 = construct_ising_local_term(J, 0, 0, ndim=2)
+hloc2 = g*(np.kron(X, I2)+np.kron(I2, X))/4
+
+V1 = scipy.linalg.expm(-1j*t*hloc1/2)
+V2 = scipy.linalg.expm(-1j*t*hloc2)
+V3 = scipy.linalg.expm(-1j*t*hloc1/2)
+V4 = scipy.linalg.expm(-1j*t*hloc2)
+YZ = np.kron(Y, Z)
+
+perms_v, perms_h = get_perms(Lx, Ly)
+
+
+Vlist_start = [YZ, V1, V2, V1, YZ, YZ, V3, V4, V3, YZ]
+Vlist_reduced = [V1, V2, V1, V3, V4, V3]
+perms_extended = [[perms_v[0]]] + [perms_v]*3 + [[perms_v[0]], [perms_h[0]]] +\
+                    [perms_h]*3 + [[perms_h[0]]]
+perms_ext_reduced = [perms_v]*3  + [perms_h]*3
+control_layers = [0, 4, 5, 9] 			# 4 control layers
+
+
+with h5py.File(f"./results/tfim2d_ccU_SPARSE_103_Lx4Ly4_t0.25_layers10_niter5_rS1_2hloc.hdf5", "r") as f:
+    Vlist_start =  f["Vlist"][:]
+
+# 12 layers with 6 being controlled, 9 parameters in total.
+state = random_statevector(2**L).data
+print("Trotter error of the starting point: ", (np.linalg.norm(ansatz_sparse(
+    Vlist_start, L, perms_extended, state) - expm_multiply(
+    1j * t * hamil, state), ord=2) + np.linalg.norm(ansatz_sparse(
+    Vlist_reduced, L, perms_ext_reduced, state) - expm_multiply(
+    -1j * t * hamil, state), ord=2))/2)
+
+print('fidelity: ', (state_fidelity(ansatz_sparse(Vlist_start, L, perms_extended, state), expm_multiply(
+    1j * t * hamil, state)) + state_fidelity(ansatz_sparse(Vlist_reduced, L, perms_ext_reduced, state), expm_multiply(
+    -1j * t * hamil, state)))/2)
+
+
+from optimize_sparse import optimize, err
+
+Vlist, f_iter, err_iter = optimize(L, hamil, t, Vlist_start, perms_extended, perms_reduced=perms_ext_reduced, 
+                                   control_layers=control_layers, rS=1, niter=3)
+plt.plot(err_iter)
+plt.yscale('log')
+print(err_iter[-1])
+
+
+with h5py.File(f"./tfim2d_ccU_SPARSE_{J}{h}{g}_Lx{Lx}Ly{Ly}_t{t}_layers{len(Vlist_start)}_niter8_rS1_2hloc.hdf5", "w") as f:
+    f.create_dataset("Vlist", data=Vlist)
+    f.create_dataset("f_iter", data=f_iter)
+    f.create_dataset("err_iter", data=err_iter)
+    f.attrs["L"] = L
+    f.attrs["t"] = float(t)
