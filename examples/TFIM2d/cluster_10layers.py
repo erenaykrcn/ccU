070fb8c7005bcbe93f4d600a22acc8fa2266e42c
Erenay Karacan <erenaykaracan@student-net-vpn-4008.intern.ethz.ch>
2025-07-26 21:49:00 +0200

    add the 1/3 compression plot

--- examples/TFIM2d/cluster_10layers.py
+++ examples/TFIM2d/cluster_10layers.py
@@ -55,6 +55,10 @@ perms_extended = [[perms_v[0]]] + [perms_v]*3 + [[perms_v[0]], [perms_h[0]]] +\
 perms_ext_reduced = [perms_v]*3  + [perms_h]*3
 control_layers = [0, 4, 5, 9] 			# 4 control layers
 
+
+with h5py.File(f"./results/tfim2d_ccU_SPARSE_103_Lx4Ly4_t0.25_layers10_niter5_rS1_2hloc.hdf5", "r") as f:
+    Vlist_start =  f["Vlist"][:]
+
 # 12 layers with 6 being controlled, 9 parameters in total.
 state = random_statevector(2**L).data
 print("Trotter error of the starting point: ", (np.linalg.norm(ansatz_sparse(
@@ -71,13 +75,13 @@ print('fidelity: ', (state_fidelity(ansatz_sparse(Vlist_start, L, perms_extended
 from optimize_sparse import optimize, err
 
 Vlist, f_iter, err_iter = optimize(L, hamil, t, Vlist_start, perms_extended, perms_reduced=perms_ext_reduced, 
-                                   control_layers=control_layers, rS=1, niter=5)
+                                   control_layers=control_layers, rS=1, niter=3)
 plt.plot(err_iter)
 plt.yscale('log')
 print(err_iter[-1])
 
 
-with h5py.File(f"./tfim2d_ccU_SPARSE_{J}{h}{g}_Lx{Lx}Ly{Ly}_t{t}_layers{len(Vlist_start)}_niter5_rS1_2hloc.hdf5", "w") as f:
+with h5py.File(f"./tfim2d_ccU_SPARSE_{J}{h}{g}_Lx{Lx}Ly{Ly}_t{t}_layers{len(Vlist_start)}_niter8_rS1_2hloc.hdf5", "w") as f:
     f.create_dataset("Vlist", data=Vlist)
     f.create_dataset("f_iter", data=f_iter)
     f.create_dataset("err_iter", data=err_iter)
