5b532129e50ae38ae913faec43302cd599ca6db6
Erenay Karacan <erenaykaracan@Erenays-MacBook-Pro.local>
2025-09-19 10:29:44 +0200

    add latest cluster scripts

--- examples/triangularTFIM2d/cluster.py
+++ examples/triangularTFIM2d/cluster.py
@@ -32,6 +32,7 @@ ground_state = eigenvectors_sort[:, 0]
 
 hloc1 = construct_ising_local_term(J, 0, 0, ndim=2)
 hloc2 = g*(np.kron(X, I2)+np.kron(I2, X))/6
+hloc = hloc1 + hloc2
 perms_1 = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], [1, 2, 3, 0, 5, 6, 7, 4, 9, 10, 11, 8, 13, 14, 15, 12]]
 perms_2 = [[0, 5, 10, 15, 3, 4, 9, 14, 2, 7, 8, 13, 1, 6, 11, 12], [5, 10, 15, 0, 4, 9, 14, 3, 7, 8, 13, 2, 6, 11, 12, 1]]
 perms_3 = [[0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15], [4, 8, 12, 0, 5, 9, 13, 1, 6, 10, 14, 2, 7, 11, 15, 3]]
@@ -51,21 +52,18 @@ Z = np.array([[1, 0], [0, -1]])
 Y = np.array([[0, -1j], [1j, 0]])
 I2 = np.array([[1, 0], [0, 1]])
 
-V1 = scipy.linalg.expm(-1j*t*hloc1/4)
-V2 = scipy.linalg.expm(-1j*t*hloc2/2)
-V3 = scipy.linalg.expm(-1j*t*hloc1/2)
-V4 = scipy.linalg.expm(-1j*t*hloc2)
+V1 = scipy.linalg.expm(-1j*t*hloc1/2)
+V2 = scipy.linalg.expm(-1j*t*hloc2)
+V = scipy.linalg.expm(-1j*t*hloc)
 YZ = np.kron(Y, Z)
 
-Vlist_start = [YZ, V1, V2, V1, YZ]*2 +  [YZ, V3, V4, V3, YZ]  + [YZ, V1, V2, V1, YZ]*2
-Vlist_reduced = [V1, V2, V1]*2 +  [V3, V4, V3]  + [V1, V2, V1]*2
+Vlist_start =  [YZ, V, YZ, YZ, V, YZ, YZ, V, YZ]
+Vlist_reduced = [V, V, V]
 
-perms_extended = [[perms_1[0]]] + [perms_1]*3 + [[perms_1[0]], [perms_2[0]]] +\
-                    [perms_2]*3 + [[perms_2[0]], [perms_3[0]]] + [perms_3]*3 + [[perms_3[0]], [perms_2[0]]] +\
-                    [perms_2]*3 + [[perms_2[0]], [perms_1[0]]] + [perms_1]*3 + [[perms_1[0]]]
-perms_ext_reduced = [perms_1]*3  + [perms_2]*3 + [perms_3]*3 + [perms_2]*3 + [perms_1]*3
-control_layers = [0, 4, 5, 9, 10, 14, 15, 19, 20, 24] # 10 control layers
-
+perms_extended = [[perms_1[0]]] + [perms_1] + [[perms_1[0]], [perms_2[0]]] +\
+                    [perms_2] + [[perms_2[0]], [perms_3[0]]] + [perms_3] + [[perms_3[0]]] 
+perms_ext_reduced = [perms_1]  + [perms_2] + [perms_3]
+control_layers = [0, 2, 3, 5, 6, 8]
 
 # 12 layers with 6 being controlled, 9 parameters in total.
 state = random_statevector(2**L).data
@@ -81,6 +79,9 @@ from optimize_sparse import optimize, err
 
 Vlist, f_iter, err_iter = optimize(L, hamil, t, Vlist_start, perms_extended, perms_reduced=perms_ext_reduced, 
                                    control_layers=control_layers, rS=1, niter=5)
+plt.plot(err_iter)
+plt.yscale('log')
+print(err_iter[-1])
 
 
 with h5py.File(f"./triangularTFIM_ccU_SPARSE_{J}{h}{g}_Lx{Lx}Ly{Ly}_t{t}_layers{len(Vlist_start)}_niter5_rS1_2hloc.hdf5", "w") as f:
@@ -89,4 +90,3 @@ with h5py.File(f"./triangularTFIM_ccU_SPARSE_{J}{h}{g}_Lx{Lx}Ly{Ly}_t{t}_layers{
     f.create_dataset("err_iter", data=err_iter)
     f.attrs["L"] = L
     f.attrs["t"] = float(t)
-
