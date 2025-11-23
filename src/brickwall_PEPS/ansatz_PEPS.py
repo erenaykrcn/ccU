2ab1e5ae7924e5020ba46f2d9c22f6d5cfe48e58
Erenay Karacan <erenaykaracan@student-net-vpn-2374.intern.ethz.ch>
2025-11-23 11:32:33 +0100

    add PEPS implementation

--- src/brickwall_PEPS/ansatz_PEPS.py
+++ src/brickwall_PEPS/ansatz_PEPS.py
@@ -26,8 +26,8 @@ def ansatz_PEPS_grad(V, L, v, w, perms, max_bond_dim, chi_overlap, n_workers=1):
         
         def _pair_grad(i):
             with open("GRAD_log.txt", "a") as file:
-            forile.write(f"One call in ansatz_PEPS_grad\n")
-            
+                file.write(f"One call in ansatz_PEPS_grad\n")
+
             v_working = v1.copy()
             k, l = perm[2 * i], perm[2 * i + 1]
             for j in range(i):
