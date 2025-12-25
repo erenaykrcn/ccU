b9d335978e4e0930fc89a8601ad5a306508b277a
Erenay Karacan <erenaykaracan@student-net-vpn-3358.intern.ethz.ch>
2025-12-25 22:49:45 +0100

    ver

--- examples/Kagome/cluster/optimization.py
+++ examples/Kagome/cluster/optimization.py
@@ -17,6 +17,8 @@ from qiskit.quantum_info import state_fidelity
 result_string = None
 niter = 20
 t = 0.1
+layers = 36
+
 
 def bonds_from_perms(perms):
     bonds = []
@@ -98,32 +100,53 @@ Z = np.array([[1, 0], [0, -1]])
 Y = np.array([[0, -1j], [1j, 0]])
 YZ = np.kron(Y, Z)
 I2 = np.array([[1, 0], [0, 1]])
+XZ = np.kron(X, Z)
+XY = np.kron(X, Y)
+ZY = np.kron(Z, Y)
 
 
 state = np.array(random_statevector(2**L).data)
 hloc1 = construct_heisenberg_local_term((J[0], 0   ,    0), (0, h[1],       0), ndim=2)
 hloc2 = construct_heisenberg_local_term((0   ,    J[1], 0), (0, 0, h[2]   ), ndim=2)
 hloc3 = construct_heisenberg_local_term((0   , 0   , J[2]), (h[0], 0,       0), ndim=2)
-V1 = scipy.linalg.expm(-1j*t*hloc1)
-V2 = scipy.linalg.expm(-1j*t*hloc2)
-V3 = scipy.linalg.expm(-1j*t*hloc3)
-X = np.array([[0, 1], [1, 0]])
-Z = np.array([[1, 0], [0, -1]])
-Y = np.array([[0, -1j], [1j, 0]])
-I2 = np.array([[1, 0], [0, 1]])
-XZ = np.kron(X, Z)
-XY = np.kron(X, Y)
-ZY = np.kron(Z, Y)
-Vlist_start = [XZ, V1, V1, XZ]*3 + [XY, V2, V2, XY]*3  + [ZY, V3, V3, ZY]*3
-Vlist_reduced = [V1]*6 + [V2]*6 + [V3]*6
 
 
-perms_extended = [[perms_1[0]]] + [[perms_1[0]]]+ [[perms_1[1]]] + [[perms_1[0]], [perms_2[0]]] +\
-      [[perms_2[0]]]+ [[perms_2[1]]]  + [[perms_2[0]], [perms_3[0]]] + [[perms_3[0]]]+ [[perms_3[1]]]  + [[perms_3[0]]]
-perms_extended = perms_extended*3
-perms_ext_reduced =  [[perms_1[0]]]+ [[perms_1[1]]]  +  [[perms_2[0]]]+ [[perms_2[1]]]  +  [[perms_3[0]]]+ [[perms_3[1]]]
-perms_ext_reduced = perms_ext_reduced*3
-non_control_layers = []
+if layers == 36:
+    V1 = scipy.linalg.expm(-1j*t*hloc1)
+    V2 = scipy.linalg.expm(-1j*t*hloc2)
+    V3 = scipy.linalg.expm(-1j*t*hloc3)
+    Vlist_start = [XZ, V1, V1, XZ]*3 + [XY, V2, V2, XY]*3  + [ZY, V3, V3, ZY]*3
+    Vlist_reduced = [V1]*6 + [V2]*6 + [V3]*6
+
+    perms_extended = [[perms_1[0]]] + [[perms_1[0]]]+ [[perms_1[1]]] + [[perms_1[0]], [perms_2[0]]] +\
+          [[perms_2[0]]]+ [[perms_2[1]]]  + [[perms_2[0]], [perms_3[0]]] + [[perms_3[0]]]+ [[perms_3[1]]]  + [[perms_3[0]]]
+    perms_extended = perms_extended*3
+    perms_ext_reduced =  [[perms_1[0]]]+ [[perms_1[1]]]  +  [[perms_2[0]]]+ [[perms_2[1]]]  +  [[perms_3[0]]]+ [[perms_3[1]]]
+    perms_ext_reduced = perms_ext_reduced*3
+elif layers == 72:
+    hloc1_1 = construct_heisenberg_local_term((J[0], 0   ,    0), (0, 0,       0), ndim=2)
+    hloc1_2 = construct_heisenberg_local_term((0, 0   ,    0), (0, h[1],       0), ndim=2)
+    hloc2_1 = construct_heisenberg_local_term((0   ,    J[1], 0), (0, 0, 0   ), ndim=2)
+    hloc2_2 = construct_heisenberg_local_term((0   ,    0, 0), (0, 0, h[2]   ), ndim=2)
+    hloc3_1 = construct_heisenberg_local_term((0   , 0   , J[2]), (0, 0,       0), ndim=2)
+    hloc3_2 = construct_heisenberg_local_term((0   , 0   , 0), (h[0], 0,       0), ndim=2)
+    V1_1 = scipy.linalg.expm(-1j*t*hloc1_1)
+    V1_2 = scipy.linalg.expm(-1j*t*hloc1_2)
+    V2_1 = scipy.linalg.expm(-1j*t*hloc2_1)
+    V2_2 = scipy.linalg.expm(-1j*t*hloc2_2)
+    V3_1 = scipy.linalg.expm(-1j*t*hloc3_1)
+    V3_2 = scipy.linalg.expm(-1j*t*hloc3_2)
+    Vlist_start = [XZ, V1_1, V1_1, XZ]*3 + [XZ, V1_2, V1_2, XZ]*3 +\
+     [XY, V2_1, V2_1, XY]*3  + [XY, V2_2, V2_2, XY]*3 +\
+     [ZY, V3_1, V3_1, ZY]*3 + [ZY, V3_2, V3_2, ZY]*3
+    Vlist_reduced = [V1_1]*6 + [V1_2]*6  + [V2_1]*6 + [V2_2]*6 + [V3_1]*6 + [V3_2]*6
+
+    perms_extended = [[perms_1[0]]] + [[perms_1[0]]]+ [[perms_1[1]]] + [[perms_1[0]], [perms_2[0]]] +\
+          [[perms_2[0]]]+ [[perms_2[1]]]  + [[perms_2[0]], [perms_3[0]]] + [[perms_3[0]]]+ [[perms_3[1]]]  + [[perms_3[0]]]
+    perms_extended = perms_extended*6
+    perms_ext_reduced =  [[perms_1[0]]]+ [[perms_1[1]]]  +  [[perms_2[0]]]+ [[perms_2[1]]]  +  [[perms_3[0]]]+ [[perms_3[1]]]
+    perms_ext_reduced = perms_ext_reduced*6
+
 
 non_control_layers = []
 k = 0
